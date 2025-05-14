b1864e43a73b658f26f85834ede972ba8288d8a3
Eliane Brito <egomes.brito@gmail.com>
2025-05-14 13:48:54 +0100

    Inicializando o repositorio com os arquivos iniciais do projeto

---
+++ src/test/java/DataFactory/UsuarioDataFactory.java
@@ -0,0 +1,14 @@
+package DataFactory;
+
+import pojo.ProdutoPojo;
+import pojo.UsuarioPojo;
+
+public class UsuarioDataFactory {
+    public static UsuarioPojo criarUsuarioAdministrador () {
+        UsuarioPojo usuario = new UsuarioPojo();
+        usuario.setUsuarioLogin("admin");
+        usuario.setUsuarioSenha("admin");
+
+        return usuario;
+    }
+}
